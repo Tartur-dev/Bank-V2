f5840124e236d1f570747a0fef157ad01aafe7f4
Tartur-dev <arthurbarbera5@gmail.com>
2022-05-31 22:09:40 +0200

    Adding IO manager

---
+++ IO/Input.cs
@@ -0,0 +1,33 @@
+namespace Application.IO
+{
+    public static class Input
+    {
+        public static void Open(out string input, string writeLine, string error)
+        {
+            Console.Write(writeLine);
+
+            input = Console.ReadLine().Trim();
+
+            while (string.IsNullOrEmpty(input))
+            {
+                Console.Write($"{error}. Recommencez : ");
+                input = Console.ReadLine().Trim();
+            }
+        }
+
+        public static string OpenIf(out string input, string writeLine, string error, Func<string, bool> predicate)
+        {
+            Console.Write(writeLine);
+
+            input = Console.ReadLine().Trim();
+
+            while (string.IsNullOrEmpty(input) || !predicate(input))
+            {
+                Console.Write($"{error}. Recommencez : ");
+                input = Console.ReadLine().Trim();
+            }
+
+            return input;
+        }
+    }
+}
