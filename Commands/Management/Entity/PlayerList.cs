7f19a0dfe3305ed6695f5a42d5b0c68446faef9c
Tartur-dev <arthurbarbera5@gmail.com>
2022-06-03 15:35:19 +0200

    Adding playerlist command

---
+++ Commands/Management/Entity/PlayerList.cs
@@ -0,0 +1,29 @@
+﻿using BankV2.Commands.Common;
+using BankV2.Entities;
+using BankV2.Manager;
+
+namespace BankV2.Commands.Management.Entity;
+
+public class PlayerList : Command
+{
+    private readonly ConsoleApplication _app;
+    
+    public PlayerList(ConsoleApplication app) : base("playerlist", "Obtenir la liste des joueurs connectés", null)
+    {
+        _app = app;
+    }
+
+    public override bool Run(Player player, IReadOnlyList<string> providedArguments)
+    {
+        if (providedArguments.Count != 0) return false;
+        
+        System.Console.WriteLine("Les joueurs connectés sont :");
+
+        foreach (var target in _app.PlayerList)
+        {
+            System.Console.WriteLine($"- {target.Name}");
+        }
+
+        return true;
+    }
+}
