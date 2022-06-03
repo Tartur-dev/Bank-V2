1ec1179dfe9897735106f036d6a5b354f9197ac3
Tartur-dev <arthurbarbera5@gmail.com>
2022-06-03 15:33:38 +0200

    Adding the exit command

---
+++ Commands/Management/Console/Exit.cs
@@ -0,0 +1,23 @@
+﻿using BankV2.Commands.Common;
+using BankV2.Entities;
+using BankV2.Manager;
+
+namespace BankV2.Commands.Management.Console;
+
+public class Exit : Command
+{
+    private ConsoleApplication _app;
+    
+    public Exit(ConsoleApplication app) : base("exit", "Quitter l'invite de commandes", null)
+    {
+        _app = app;
+    }
+
+    public override bool Run(Player player, IReadOnlyList<string> providedArguments)
+    {
+        if (providedArguments.Count != 0) return false;
+
+        _app.Running = false;
+        return true;
+    }
+}
