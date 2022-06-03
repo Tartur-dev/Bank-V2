c262c54e92eb5e5d60ef54991d1e80247e08e5e0
Tartur-dev <arthurbarbera5@gmail.com>
2022-06-03 15:33:03 +0200

    Adding the clear command

---
+++ Commands/Management/Console/Clear.cs
@@ -0,0 +1,19 @@
+﻿using BankV2.Commands.Common;
+using BankV2.Entities;
+
+namespace BankV2.Commands.Management.Console;
+
+public class Clear : Command
+{
+    public Clear() : base("clear", "Vider la console", null)
+    {
+    }
+
+    public override bool Run(Player player, IReadOnlyList<string> providedArguments)
+    {
+        if (providedArguments.Count != 0) return false;
+        
+        System.Console.Clear();
+        return true;
+    }
+}
