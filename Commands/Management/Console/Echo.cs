3ef8046435b4edc76fce333d128cafe6161a74d2
Tartur-dev <arthurbarbera5@gmail.com>
2022-06-03 15:33:20 +0200

    Adding the echo command

---
+++ Commands/Management/Console/Echo.cs
@@ -0,0 +1,23 @@
+﻿using BankV2.Commands.Common;
+using BankV2.Entities;
+
+namespace BankV2.Commands.Management.Console;
+
+public class Echo : Command
+{
+    public Echo() : base("echo", "Imprimer du texte en console", new ArgumentsBuilder().Add("texte").Build())
+    {
+    }
+
+    public override bool Run(Player player, IReadOnlyList<string> providedArguments)
+    {
+        if (providedArguments.Count == 0) return false;
+
+        foreach (var argument in providedArguments)
+        {
+            System.Console.Write($"{argument} ");
+        }
+
+        return true;
+    }
+}
